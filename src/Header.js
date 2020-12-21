adf3e73ca43449e1aa27b990315eb755dc66510f
akshitagarg15 <akshitagarg15@gmail.com>
2020-12-21 20:39:39 +0530

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './header.css'
+import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+function Header() {
+    return (
+      <div className="header">
+        <div className="header__left">
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+            alt=""
+          />
+          <div className="header__search">
+            <SearchIcon />
+            <input type="text" />
+          </div>
+        </div>
+        <div className="header__right">
+          
+        </div>
+      </div>
+    );
+}
+
+export default Header
